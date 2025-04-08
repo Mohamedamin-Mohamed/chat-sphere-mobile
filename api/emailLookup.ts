6df3f5deb7c9e33e0db9a855337803f55c2b2930
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-07 20:24:25 -0500

    refractor: changed the hard coded url to the global one

--- api/emailLookup.ts
+++ api/emailLookup.ts
@@ -1,6 +1,6 @@
 const emailLookup = async (email: string, controller: AbortController) => {
     const url = process.env.EXPO_PUBLIC_BACKEND_URL
-    console.log('Url is ', url)
+
     return await fetch(`${url}/auth/email_lookup/generate_code?email=${email}`, {
         method: 'GET',
         signal: controller.signal
