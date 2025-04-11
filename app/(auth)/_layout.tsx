6f2b4ca21349a290b202d596fc7b819922486543
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-11 12:29:25 -0500

    feat: simplified screens rendering in the layout file

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,13 +2,7 @@ import {Stack} from 'expo-router';
 
 const AuthLayout = () => {
     return (
-        <Stack screenOptions={{title: ''}}>
-            <Stack.Screen name="Welcome" options={{headerTitle: ''}}/>
-            <Stack.Screen name="SignIn" options={{headerTitle: '', headerShown: false}}/>
-            <Stack.Screen name="SignUp" options={{headerTitle: '', headerShown: false}}/>
-            <Stack.Screen name="EmailLookup" options={{headerTitle: '', headerShown: false}}/>
-            <Stack.Screen name="GetStarted" options={{headerTitle: 'Get started', headerTitleAlign: "left"}}/>
-        </Stack>
+        <Stack screenOptions={{headerShown: false}}/>
     )
 }
 
