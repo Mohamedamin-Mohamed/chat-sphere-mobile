969d327ae624564238c89e8f96a4f6d5d77e9785
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-11 12:28:42 -0500

    feat: wrapped chid components with Provider from react-redux

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import {Stack} from 'expo-router';
+import store, {persistor} from "../redux/store";
+import {PersistGate} from "redux-persist/integration/react";
+import {Provider} from "react-redux";
 
 const RootLayout = () => {
     return (
-        <Stack screenOptions={{headerShown: false, headerTitle: ''}}>
-            <Stack.Screen name="index" options={{headerShown: false, headerTitle: ''}}/>
-        </Stack>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={null}>
+                <Stack screenOptions={{headerShown: false, headerTitle: ''}}>
+                    <Stack.Screen name="index" options={{headerShown: false, headerTitle: ''}}/>
+                </Stack>
+            </PersistGate>
+        </Provider>
+
     )
 }
 
