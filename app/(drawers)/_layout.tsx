8363e42e1481181ec60d92bf6cf064924ce070e7
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-11 12:27:20 -0500

    feat: changed layout to be tabs instead of drawers

--- app/(drawers)/_layout.tsx
+++ app/(drawers)/_layout.tsx
@@ -1,21 +1,48 @@
-import {Drawer} from "expo-router/drawer";
-import CustomDrawerContent from "../../components/CustomDrawerContent";
-import {useNavigation} from "expo-router";
+import {Tabs, useNavigation} from "expo-router";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const DrawerLayout = () => {
     const navigation = useNavigation()
     return (
-        <Drawer
-            drawerContent={props => <CustomDrawerContent {...props} />}
+        // <Drawer
+        //     drawerContent={props => <CustomDrawerContent {...props} />}
+        //     screenOptions={({route}) => ({
+        //         headerShown: true,
+        //         drawerStyle: {
+        //             backgroundColor: "#f5f5f5"
+        //         }
+        //
+        //     })}
+        // >
+        // </Drawer>
+        <Tabs
             screenOptions={({route}) => ({
-                headerShown: true,
-                drawerStyle: {
-                    backgroundColor: "#f5f5f5"
-                }
+                tabBarActiveTintColor: "#085bd8",
+                tabBarLabelStyle: {fontSize: 14},
+                tabBarStyle: {paddingHorizontal: 30},
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarIcon: ({color, size}) => {
+                    let iconName;
 
+                    if (route.name === 'profile') {
+                        iconName = 'person';
+                    } else if (route.name === 'chat') {
+                        iconName = 'chat-bubble';
+                    } else {
+                        iconName = 'explore';
+                    }
+
+                    return <Icon name={iconName} color={color} size={size}/>;
+                },
             })}
         >
-        </Drawer>
+            <Tabs.Screen name="chat"/>
+            <Tabs.Screen name="discover"/>
+            <Tabs.Screen name="profile"/>
+        </Tabs>
+
+
     );
 };
 
