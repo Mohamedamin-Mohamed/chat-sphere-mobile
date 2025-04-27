86df8d2a991c8f2dd1baf9534dd7c6e15d5f1fbb
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-26 21:45:28 -0500

    feat: add new api for interaction with gpt api

---
+++ api/askChatGPT.ts
@@ -0,0 +1,24 @@
+const askChatGPT = async (question: string) => {
+    const url = process.env.EXPO_PUBLIC_CHATGPT_CHAT_URL
+    const apiKey = process.env.EXPO_PUBLIC_OPENAI_APIKEY
+
+    const messages = [
+        {role: 'system', content: `Today is ${new Date().toLocaleDateString('en-US', {dateStyle: 'full'})}.`},
+        {role: 'user', content: question}
+    ]
+
+    return await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            'model': 'gpt-3.5-turbo',
+            messages: messages,
+            'max_tokens': 20
+        })
+    })
+}
+
+export default askChatGPT
