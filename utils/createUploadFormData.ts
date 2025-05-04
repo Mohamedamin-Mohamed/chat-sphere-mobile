9253b415fd9a1b66d145b4ab6d1e66df6aff221c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-03 22:47:47 -0500

    feat: add new function that generates formData of a request

---
+++ utils/createUploadFormData.ts
@@ -0,0 +1,20 @@
+import {UpdateProfileType} from "../types/types";
+
+const createUploadFormData = (request: UpdateProfileType) => {
+    const formData = new FormData();
+    formData.append("email", request.email)
+    formData.append("phoneNumber", request.phoneNumber)
+    formData.append("name", request.name ?? '')
+    formData.append("newEmail", request.newEmail ?? '')
+    formData.append("bio", request.bio ?? '')
+    if (request.profilePictureDetails !== null) {
+        formData.append("profilePictureDetails", {
+            name: request.profilePictureDetails?.fileName,
+            uri: request.profilePictureDetails?.file.uri,
+            type: request.profilePictureDetails?.file.mimeType,
+        } as any)
+    }
+    return formData
+}
+
+export default createUploadFormData
