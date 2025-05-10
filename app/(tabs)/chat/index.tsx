a67d03636c06c7f17d1b69592d3a03e1b3e10f40
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:28:21 -0500

    feat: add new search icon

--- app/(tabs)/chat/index.tsx
+++ app/(tabs)/chat/index.tsx
@@ -27,11 +27,11 @@ const Page = () => {
                     <Text style={styles.chatText}>Chat</Text>
                     <View style={styles.sideView}>
                         <TouchableOpacity style={styles.buttons} activeOpacity={0.8}>
-                            <MaterialCommunityIcons name="qrcode" size={30}/>
+                            <MaterialCommunityIcons name="qrcode" size={30} color="#4F46E5"/>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.buttons} activeOpacity={0.8}
                                           onPress={() => router.push('search')}>
-                            <Icon name="add" size={30} color="gray"/>
+                            <Icon name="search" size={30} color="#4F46E5"/>
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -83,7 +83,8 @@ const styles = StyleSheet.create({
     },
     chatText: {
         fontSize: 30,
-        fontWeight: "600"
+        fontWeight: "600",
+        color: "#4F46E5"
     },
     sideView: {
         flexDirection: 'row',
