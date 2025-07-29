bd6c93d2e5f80d331aa815865a4f671d7c235e96
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-07-28 21:25:22 -0500

    chore: add comment

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
 import {Redirect} from "expo-router"
+import {useSelector} from "react-redux";
+import {RootState} from "../types/types";
 
 const Index = () => {
-
+    //here we should check for token instead but now lets just have it as placeholder
+    const user = useSelector((state: RootState) => state.userInfo)
+    if (user) {
+        return <Redirect href="/chat"/>
+    }
     return (
         <Redirect href="/Welcome"/>
     )
